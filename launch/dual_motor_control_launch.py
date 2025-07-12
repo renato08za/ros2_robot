6fa49acc85c14a734741d5e4072a361921b021a7
renato08za <renatosantossousa23@gmail.com>
2025-07-12 08:59:30 -0300

    Descrição das mudanças

---
+++ launch/dual_motor_control_launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='control_motor',
+            executable='encoder_publisher',
+            name='encoder_publisher',
+            output='screen',
+        ),
+        Node(
+            package='control_motor',
+            executable='dual_motor_pid_node',
+            name='dual_motor_pid_node',
+            output='screen',
+        ),
+    ])
