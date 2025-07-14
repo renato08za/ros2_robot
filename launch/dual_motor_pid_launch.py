bda7825672af715870927cc20782fae115db1c6c
renato08za <renatosantossousa23@gmail.com>
2025-07-14 07:49:19 -0300

    Minha alteração no PC

--- launch/dual_motor_pid_launch.py
+++ launch/dual_motor_pid_launch.py
@@ -5,14 +5,14 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='control_motor',
-            executable='encoder_publisher',
-            name='encoder_publisher',
-            output='screen',
+            executable='simple_dual_motor_control',
+            name='simple_dual_motor_control',
+            output='screen'
         ),
         Node(
             package='control_motor',
-            executable='dual_motor_pid_node',
-            name='dual_motor_pid_nod',
-            output='screen',
+            executable='pid_motor_control',
+            name='pid_motor_control',
+            output='screen'
         ),
     ])
